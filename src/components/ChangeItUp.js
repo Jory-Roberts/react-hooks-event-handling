293358b3d31a0c16e52e2f8b78379b6183bc20a1
Jory Roberts <robertsjory13@gmail.com>
2023-05-22 11:00:45 -0500

    changes format seetings

--- src/components/ChangeItUp.js
+++ src/components/ChangeItUp.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from "react"
 
 function ChangeItUp() {
     function handleChange(event) {
@@ -8,19 +8,16 @@ function ChangeItUp() {
     return (
         <div>
             <input
-                type='text'
-                name='search'
+                type="text"
+                name="search"
                 onChange={handleChange}
-                placeholder='Enter search term...'
+                placeholder="Enter search term..."
             />
 
-            <select
-                name='filter'
-                onChange={handleChange}
-            >
-                <option value='all'>Select a filter...</option>
-                <option value='completed'>Completed</option>
-                <option value='incomplete'>Incomplete</option>
+            <select name="filter" onChange={handleChange}>
+                <option value="all">Select a filter...</option>
+                <option value="completed">Completed</option>
+                <option value="incomplete">Incomplete</option>
             </select>
         </div>
     )
